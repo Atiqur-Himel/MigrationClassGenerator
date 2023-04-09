7625963d145e7ec582e45cdbdaccdf5dfcf70139
Atiqur-Himel <atiqur.himel@selise.ch>
2023-04-09 10:00:52 +0600

    all

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+using MigrationClassGenerator;
+using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+
+
+var fileOption = new Option<string?>(
+           name: "--create",
+           description: "Here Provide your class name.");
+
+var rootCommand = new RootCommand("Sample app for System.CommandLine");
+rootCommand.AddOption(fileOption);
+
+rootCommand.SetHandler((file) =>
+{
+    CreateClass(file!);
+},
+    fileOption);
+
+return await rootCommand.InvokeAsync(args);
+
+
+
+
+
+static void CreateClass (string EntityName)
+{
+    //string EntityName = Console.ReadLine();
+    EntityName = EntityName + DateTime.Now.ToString("yyyyMMddHHmmss");
+    string projectFilesource = @"C:\MigrationClassGenerator\ProjectNameSpace.txt";
+    StreamReader projectNamereader = new StreamReader(projectFilesource);
+    string ProjectName = projectNamereader.ReadLine();
+    Engine program = new Engine(EntityName, ProjectName);
+    program.Main();
+}
